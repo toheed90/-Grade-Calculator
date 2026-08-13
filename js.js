22af4e648e8da239d255fed579db50d059a3833d
Toheed hashim <toheedhashim99@gmail.com>
2026-08-13 18:06:06 +0500

    Grade Calculator
     Grade Calculator

---
+++ js.js
@@ -0,0 +1,79 @@
+// let user = prompt("Enter ");
+// for( let i = 0; i <= 5; i++) {
+//     console.log(i);
+// }
+
+
+// let user = prompt("Enter ");
+// for( let user = 0; i <= 5;) {
+//     console.log(i);
+// }
+
+
+
+// for (let i = 0; i <= 50 ; i--) {
+//     console.log(i);
+// }
+
+
+// let citytocheck = prompt("Enter a city name")
+// let cleanestcities = ["New York", "San Francisco", "Boston"]
+// for  (var i = 0; i <= 4; i++) {
+// if ( citytocheck === cleanestcities[i]){
+//     console.log
+//     ("its find")
+// }
+// }
+
+// for (let i = +prompt("Enter a number"); i <= 10; i++) {     
+//     console.log(i  +  "x"  + i * 1 + " = " +    i);
+// }
+
+
+// let s = prompt("Enter a number");
+// for (let i = 1; i <= 10; i++ ) {     
+//     console.log( s  +  " x "  +  i + " = "  + i * s );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let marks = Number(prompt("Apne marks enter karein (0-100):"));
+
+let grade;
+
+if (marks >= 90) {
+    grade = "A+";
+} else if (marks >= 80) {
+    grade = "A";
+} else if (marks >= 70) {
+    grade = "B";
+} else if (marks >= 60) {
+    grade = "C";
+} else {
+    grade = "Fail";
+}
+
+alert("Aapka Grade hai: {grade}");
+
+
+
+
+
+
+
+
+
+
+
